3d351588ab02f453da74daef496a850b3b50c3d6
mutembeijoe <mutembeijoe@gmail.com>
2020-04-24 08:27:56 +0300

    sets  env variables and adds variadic parameters to send method

--- services/sms/sms.py
+++ services/sms/sms.py
@@ -1,11 +1,11 @@
 import africastalking
+import os
 
 class SMS:
     def __init__(self):
         # Set your app credentials
-        
-        self.username = "YOUR_USERNAME"
-        self.api_key = "YOUR_API_KEY"
+        self.username = os.getenv("AFRICASTALKING_USERNAME")
+        self.api_key =  os.getenv("AFRICASTALKING_API_KEY")
 
         # Initialize the SDK
         africastalking.initialize(self.username, self.api_key)
@@ -13,12 +13,12 @@ class SMS:
         # Get the SMS service
         self.sms = africastalking.SMS
 
-    def send(self):
+    def send(self, *recipients):
         # Set the numbers you want to send to in international format
-        recipients = ["+254713YYYZZZ", "+254733YYYZZZ"]
-
+        recipients = recipients
+        
         # Set your message
-        message = "I'm a lumberjack and it's ok, I sleep all night and I work all day";
+        message = "I'm a lumberjack and it's ok, I sleep all night and I work all day"
 
         # Set your shortCode or senderId
         sender = "shortCode or senderId"
@@ -31,4 +31,4 @@ class SMS:
             print ('Encountered an error while sending: %s' % str(e))
 
 if __name__ == '__main__':
-    SMS().send()
+    SMS().send(["+254713YYYZZZ", "+254733YYYZZZ"])
